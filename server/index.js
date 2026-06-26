ad8a4c9c9dd016f7e4d447aa0a1e1a29a03d894e
Chidile (Afro-Tech Boss) <afrotechboss@yahoo.com>
2026-06-26 21:46:18 +0100

    Update CORS allowed origins to include beta and preview subdomains

--- server/index.js
+++ server/index.js
@@ -31,6 +31,8 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'https://certo.ng',
   'https://www.certo.ng',
+  'https://beta.certo.ng',
+  'https://preview.certo.ng',
   'http://localhost:3000',
   'http://localhost:5173',
 ];
